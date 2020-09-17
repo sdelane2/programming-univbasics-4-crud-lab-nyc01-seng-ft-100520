35ab5d1c05622bcd7abe08cc4bfbf2c794f26b37
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-17 00:04:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   friends = ["Sean", "Billy", "Christina", "Julia"]
-  friends.unshift["Jeff"]
+  friends.unshift("Jeff")
 end
 
 def remove_element_from_end_of_array(array)
